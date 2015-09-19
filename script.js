85de33575cd16b0c14d0c9533155d13a3629df80
unknown <henryhs.yoo@gmail.com>
2015-09-18 23:00:07 -0700

    added comments

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
+	//marks ingredients when bought
 	$('ul li').click(function() {
 	$(this).css('text-decoration', 'underline');
 	});
-
+	//use can track of direction
 	$('section ol li').mouseover(function() {
 		$(this).css('text-decoration', 'underline');
 	});
 	$('section ol li').mouseleave(function() {
 		$(this).css('text-decoration', 'none');
 	});
-
+	//button for hiding all the pictures
 	$('button').click(function() {
 		$('img').fadeOut();
 	});
